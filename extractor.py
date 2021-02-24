64f589e665ba55d43eb83589f590a9e02d041bf4
nguyenkdang <57109202+nguyenkdang@users.noreply.github.com>
2021-02-24 00:57:35 -0800

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -4,42 +4,57 @@ except ImportError:
     import Image
 
 import pytesseract, sys, os
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
 class ocrCore():
-    def __init__(self, imgPath, config, lang):
+    def __init__(self, imgPath, lang, oem=3, psm=12):
         self.img =  Image.open(imgPath)
-        self.config = config
+        self.psm = psm
+        self.oem = oem
         self.lang = lang
     
-    def getString(self, config=None, lang=None):
+    def getString(self):
         ## This function will handle the core OCR processing of images.
         ## We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
-        configTemp = self.config
-        langTemp = self.lang
-        if config != None: configTemp = config
-        if lang != None: langTemp = lang
-        return pytesseract.image_to_string(self.img, lang=langTemp, config=configTemp)
+        config = '--oem {} --psm {}'.format(self.oem, self.psm)
+        return pytesseract.image_to_string(self.img, lang=self.lang, config=config)
     
-    def getData(self, config=None, lang=None):
-        configTemp = self.config
-        langTemp = self.lang
-        if config != None: configTemp = config
-        if lang != None: langTemp = lang
-        return pytesseract.image_to_boxes(self.img, lang=langTemp, config=configTemp)
+    def getData(self):
+        config = '--oem {} --psm {}'.format(self.oem, self.psm)
+        return pytesseract.image_to_data(self.img, lang=self.lang, config=config, output_type= pytesseract.Output.DICT)
     
-    def getBox(self, config=None, lang=None):
-        configTemp = self.config
-        langTemp = self.lang
-        if config != None: configTemp = config
-        if lang != None: langTemp = lang
-        return pytesseract.image_to_boxes(self.img, lang=langTemp, config=configTemp)
+    def getBox(self):
+        config = '--oem {} --psm {}'.format(self.oem, self.psm)
+        return pytesseract.image_to_boxes(self.img, lang=self.lang, config=config)
+    
+    def exportBoxes(self):
+        fn, fe = os.path.splitext(self.img.filename)
+        savePath = fn + '_' + str(self.oem) + '-' + str(self.psm) + fe        
+        fig = plt.figure(frameon=False)
+        ax = fig.add_axes([0, 0, 1, 1])
+        ax.imshow(self.img)
+        ax.axis('off')
+        data = self.getData()
+        for i in range(len(data['text'])):
+            #text = data['text'][i] 
+            left = data['left'][i]
+            top = data['top'][i]
+            width = data['width'][i]
+            height = data['height'][i]
+            rect = patches.Rectangle((left, top), width, height, linewidth=1, edgecolor='r', facecolor='none')
+            ax.add_patch(rect)
+        
+        plt.savefig(savePath, bbox_inches='tight', pad_inches=0.0, dpi=200)
 
 
-print(pytesseract.get_languages())
-path = os.path.join(sys.path[0], '5.jpg')
-config = r'--oem 3 --psm 1'
-lang='eng'
-ocr = ocrCore(path, config, lang)
-print(ocr.getString())
+#print(pytesseract.get_languages())
+path = os.path.join(sys.path[0], 'raw2.jpg')
+lang='jpn+jpn_vert'
+ocr = ocrCore(path, lang, 3, 12)
+print(ocr.getString().strip())
+#x = ocr.getData()
+#print(x)
+ocr.exportBoxes()
